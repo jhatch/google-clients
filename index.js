a5eae4305b230221800873cdb8c770811e8aa7a9
Tpt-John Hatcher <john.hatcher@teacherspayteachers.com>
2019-04-19 23:17:45 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const GoogleAPI = require('./lib/GoogleAPI');
+const GoogleCalendar = require('./lib/GoogleCalendar');
+const GoogleMail = require('./lib/GoogleMail');
+const GoogleSheet = require('./lib/GoogleSheet');
+
+module.exports = {
+  GoogleAPI,
+  GoogleCalendar,
+  GoogleMail,
+  GoogleSheet,
+};
